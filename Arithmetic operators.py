6490114c0ae1581cedc8f6c6cc57dec0a904cb77
samia2456 <67730627+samia2456@users.noreply.github.com>
2020-10-01 21:36:52 +0600

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,11 @@
+def Arithmetic_operators(a,b):
+    return a+b,a-b,a*b,a/b,a%b;
+
+add,sub,mul,div,mod = Arithmetic_operators(20,6)
+
+print(add);
+print(sub);
+print(mul);
+print(div);
+print(mod);
+
